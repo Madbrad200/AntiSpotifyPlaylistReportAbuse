d7fa1d80e729704678d4104c18b8c6f8ffe61e17
Madbrad200 <25277258+Madbrad200@users.noreply.github.com>
2022-09-20 19:11:36 +0100

    Update antispotifyreports.py

--- antispotifyreports.py
+++ antispotifyreports.py
@@ -2,11 +2,18 @@ import requests
 from requests.auth import HTTPBasicAuth
 from requests_oauthlib import OAuth2Session
 import time
+from datetime import datetime
 
 # python3.9 antispotifyreports.py
 
 client_id = "your_client_id"
 client_secret = "your_client_secret"
+
+# we need to convert the above into base64 https://www.base64encode.org/ so we can refresh the token
+# format is client_id:client_secret, they must be encoded together
+base64_encoded = "ENTER YOUR BASE64 ENCODED CLIENT_ID:CLIENT_SECRET HERE"
+
+
 redirect_uri = "https://localhost/"  # must use https, doesn't have to be a live url
 # scopes allow us to edit playlist images, edit playlists that're public/priv
 scope = ["ugc-image-upload", "playlist-modify-public", "playlist-modify-private"]
@@ -22,7 +29,8 @@ redirect_response = input("\n\nPlease paste the full redirect URL here: ")
 auth = HTTPBasicAuth(client_id, client_secret)
 
 fetch_token = spotify.fetch_token("https://accounts.spotify.com/api/token", auth=auth, authorization_response=redirect_response)
-access_token = fetch_token["access_token"]
+access_token = fetch_token["access_token"]  # expires in 3600 seconds
+refresh_token = fetch_token["refresh_token"]  # allows us to refresh the token without re-logging in
 # we have now been authenticated
 
 # enter the details of your playlist
@@ -34,6 +42,16 @@ playlist_desc = "ENTER YOUR PLAYLIST DESCRIPTION HERE"
 # e.g https://open.spotify.com/playlist/37i9dQZEVXbMDoHDwVN2tF?si=xcPmLVHRQVutEp6HgynU1Q
 # in this playlist, the playlist code is 37i9dQZEVXbMDoHDwVN2tF
 playlist_code = "ENTER PLAYLIST CODE HERE"
+
+# this function will allow us to refresh our token without re-logging in, after it expires
+def refresh_token_func():
+
+    response = requests.post('https://accounts.spotify.com/api/token', 
+                            {"grant_type": "refresh_token", 
+                             "refresh_token": refresh_token},
+                             headers={"Authorization": f"Basic {base64_encoded}"})
+    response_data = response.json()
+    return response_data["access_token"]
 
 # loop will run indefinitely 
 while True:
@@ -59,9 +77,19 @@ while True:
                                                   "description": playlist_desc,
                                                   },
                                             headers={'Authorization': f'Bearer {access_token}'})
-        print(f"{change_playlist_data}\nThe playlist has been successfully restored.")
+        
+        # token has expired and we need a new one
+        if change_playlist_data.status == 401:
+            change_playlist_data = requests.put(f'https://api.spotify.com/v1/playlists/{playlist_code}',
+                                                json={
+                                                      "name": preserved_playlist_name,
+                                                      "description": playlist_desc,
+                                                      },
+                                                headers={'Authorization': f'Bearer {access_token}'})
+            
+        print(f"({datetime.now().strftime('%H:%M:%S')}){change_playlist_data}\nThe playlist has been successfully restored.")
 
     else:
-        print(f"{playlist_data['name']}\nThe above playlist does not need to be fixed :)")
+        print(f"({datetime.now().strftime('%H:%M:%S')}){playlist_data['name']}\nThe above playlist does not need to be fixed :)")
 
     time.sleep(31)
